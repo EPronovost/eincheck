63fc088846789eca4237ce7ad104fcdd98d04532
Ethan Pronovost <epronovo1@gmail.com>
2023-11-21 17:35:52 -0800

    Add {en,dis}able_checks

---
+++ eincheck/contexts.py
@@ -0,0 +1,26 @@
+from contextlib import contextmanager
+from typing import ContextManager, Generator
+
+__EINCHECK_ENABLE_CHECKS: bool = True
+
+
+def _should_do_checks() -> bool:
+    return __EINCHECK_ENABLE_CHECKS
+
+
+@contextmanager
+def _set_enable_checks(value: bool) -> Generator[None, None, None]:
+    global __EINCHECK_ENABLE_CHECKS
+    prev = __EINCHECK_ENABLE_CHECKS
+    __EINCHECK_ENABLE_CHECKS = value
+    yield
+    assert __EINCHECK_ENABLE_CHECKS == value
+    __EINCHECK_ENABLE_CHECKS = prev
+
+
+def enable_checks() -> ContextManager[None]:
+    return _set_enable_checks(True)
+
+
+def disable_checks() -> ContextManager[None]:
+    return _set_enable_checks(False)
